a0cf54f1fbc5cc66e4b62074d7b3b8aa43206239
Aditi <adittisharma072@gmail.com>
2023-02-28 20:24:46 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'LoginPageDesign';
+  // email = new FormControl('', [Validators.required, Validators.email]);
+  // password = new FormControl('', Validators.required);
+  LoginForm: FormGroup;
+  error: String;
+  reset = false;
+  hide = true;
+  
+  ngOnInit(){
+    this.LoginForm = new FormGroup({
+        'email' : new FormControl(null, [Validators.required, Validators.pattern(/^(\d{10}|\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3}))$/)]),
+        'password': new FormControl(null, [Validators.required,Validators.minLength(6)])
+    })
+  }
+
+  onSubmit(){
+    console.log(this.LoginForm);
+    if (!this.LoginForm.valid){
+        return;
+    }
+    alert("Success !")
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MatFormFieldModule, MatLabel } from '@angular/material/form-field';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { NgModule } from '@angular/core';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatTabsModule,
+    MatSelectModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
